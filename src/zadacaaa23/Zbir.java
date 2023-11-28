335a4a01a04cb01202a7e3e3c1029199e5ed567f
Jankulovska <jankulovskasara60@gmail.com>
2023-11-28 13:33:43 +0100

    Add first java program

---
+++ src/zadacaaa23/Zbir.java
@@ -0,0 +1,13 @@
+package zadacaaa23;
+
+public class Zbir {
+public static void main(String [] args) {
+	int x=15;
+	int y=myFunc(x);
+	System.out.println(y);
+}
+public static int myFunc(int x) {
+	return x*3;
+			
+}
+}
